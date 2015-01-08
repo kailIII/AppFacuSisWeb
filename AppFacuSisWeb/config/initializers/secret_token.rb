536504c316050e6e833f308822a054d0defe93d7
rodrigo <oneosmar89@gmail.com>
2015-01-08 12:24:17 -0300

    Commit Inicial del Proyecto

---
+++ AppFacuSisWeb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppFacuSisWeb::Application.config.secret_token = '8792ab68e514f5f2f4146b392bb47d76d571a554673ec259d65d7fbc046b4b4cd0a6515cc8e9fdb5387f6909457d3cb81c493321451de2b6c0638e67120bc576'
