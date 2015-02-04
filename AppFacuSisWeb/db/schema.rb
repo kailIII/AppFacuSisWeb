4183bebf5cd43a020b12e0287ee212c01655e690
rodrigo <oneosmar89@gmail.com>
2015-02-04 12:19:52 -0300

    Se ha agregado Cargo al esquema

--- AppFacuSisWeb/db/schema.rb
+++ AppFacuSisWeb/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150204151409) do
+ActiveRecord::Schema.define(:version => 20150204151734) do
 
   create_table "alumnos", :force => true do |t|
     t.string   "ci",           :limit => 30
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(:version => 20150204151409) do
     t.string   "procedencia",  :limit => 60
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
+  end
+
+  create_table "cargos", :force => true do |t|
+    t.string   "descripcion", :limit => 60
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "carreras", :force => true do |t|
