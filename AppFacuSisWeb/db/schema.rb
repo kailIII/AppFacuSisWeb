b5e33a531a633140079a114c9e2f66cfae520721
rodrigo <oneosmar89@gmail.com>
2015-02-04 11:40:44 -0300

    Esquema para alumno

--- AppFacuSisWeb/db/schema.rb
+++ AppFacuSisWeb/db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150204142704) do
+ActiveRecord::Schema.define(:version => 20150204143801) do
+
+  create_table "alumnos", :force => true do |t|
+    t.string   "ci",           :limit => 30
+    t.string   "nombres",      :limit => 60
+    t.string   "apellidos",    :limit => 60
+    t.date     "fnac"
+    t.string   "lnac",         :limit => 60
+    t.text     "direccion",    :limit => 120
+    t.string   "telefono",     :limit => 30
+    t.string   "celular",      :limit => 30
+    t.string   "email",        :limit => 60
+    t.string   "est_civil",    :limit => 30
+    t.string   "genero",       :limit => 30
+    t.string   "nacionalidad", :limit => 60
+    t.string   "procedencia",  :limit => 60
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
 
   create_table "periodo_lectivos", :force => true do |t|
     t.integer  "periodo",    :limit => 4
