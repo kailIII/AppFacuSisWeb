26264846f3d125094ae1c3573f2633e54568ba3d
rodrigo <oneosmar89@gmail.com>
2015-02-04 11:10:59 -0300

    Se ha agregado el recurso semestre como rutas

---
+++ AppERP-Ponick/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppERPPonick::Application.config.secret_token = '3da234387f3552e0d235f49d7b09a0350db29b133eba77c06b8c658a9be58241551c969030f1287abe6040c2f8d08b1072988cd270fc533e9bc996cd2f8bc239'
