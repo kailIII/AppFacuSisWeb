4a5b8b1a8c8ba9b674ef08c7f98434e8fa8ae7f1
rodrigo <oneosmar89@gmail.com>
2015-02-04 15:29:14 -0300

    Se ha agregado el atributo accesible para los campos asociativos

--- AppFacuSisWeb/app/models/sede.rb
+++ AppFacuSisWeb/app/models/sede.rb
@@ -1,7 +1,8 @@
 class Sede < ActiveRecord::Base
+  
   belongs_to :universidad
   belongs_to :facultad
   belongs_to :ciudad
   
-  attr_accessible :nombre
+  attr_accessible :nombre, :universidad_id, :facultad_id, :ciudad_id
 end
