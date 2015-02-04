ead40bae8002c1a47b21ff7589482fe1b3534651
rodrigo <oneosmar89@gmail.com>
2015-02-04 15:16:04 -0300

    Se ha agregado tipo de asociacion para con sedes

--- AppFacuSisWeb/app/models/facultad.rb
+++ AppFacuSisWeb/app/models/facultad.rb
@@ -1,5 +1,12 @@
 class Facultad < ActiveRecord::Base
   self.table_name = "facultades"
   
+  has_many :carreras
+  has_many :sedes
+  
   attr_accessible :direccion, :email, :nombre, :telefono
+  
+  def completo
+    "#{nombre}"
+  end
 end
